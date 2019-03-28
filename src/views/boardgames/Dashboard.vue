98c449791a80f5eb3526d514cdae798c2b95e4ca
szpone <nikola.adamus@gmail.com>
2019-03-28 12:22:35 +0100

    ADD: display top three

--- src/views/boardgames/Dashboard.vue
+++ src/views/boardgames/Dashboard.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <div class="row">
+  <div class="row">
       <div class="col-lg-12">
-        <h2>Dashboard</h2>
-        <b-table :items="matches" v-if="matches"></b-table>
-        <div v-else>No matches! Please add one</div>
-        <router-link class="btn btn-primary">Add match</router-link>
+        <h1>Dashboard</h1>
+        <div class="row">
+          <div class="col-lg-3"></div>
+          <div class="col-lg-6">
+            <h3>Matches</h3>
+            <b-table striped :items="matches" :fields="fields" v-if="matches"></b-table>
+            <div v-else>No matches! Please add one</div>
+            </div>
+          </div>
+        <router-link to="/add-match" class="btn btn-primary">Add match</router-link>
+        <div class="row">
+          <div class="col-lg-3"></div>
+          <div class="col-lg-6">
+            <h3>Top 3 Games</h3>
+            <b-table striped :items="topThree"></b-table>
+          </div>
+        </div>
       </div>
-    </div>
-
-
   </div>
 </template>
 
@@ -18,6 +27,36 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data () {
+    return {
+      fields: [
+        {
+          key: 'victory',
+          label: 'Victory'
+        },
+        {
+          key: 'points',
+          label: 'Points'
+        },
+        {
+          key: 'scenario',
+          label: 'Scenario'
+        },
+        {
+          key: 'players_names',
+          label: 'Players'
+        },
+        {
+          key: 'board_game_name',
+          label: 'Board game'
+        },
+        {
+          key: 'expansion',
+          label: 'expansion'
+        }
+      ]
+    }
+  },
   computed: {
     ...mapState('boardgames', {
       matches: (state) => state.matches,
